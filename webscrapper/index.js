a01e0727dbb93afcf27ae2a23268113d235f7d87
Felipe Vieira <flpchapola@hotmail.com>
2018-08-23 00:11:08 -0300

    Testando Modos de Crawlings

---
+++ webscrapper/index.js
@@ -0,0 +1,34 @@
+//index.js
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+
+const options = {
+  uri: 'http://globoesporte.globo.com/futebol/brasileirao-serie-a/',
+  transform: function (body) {
+    return cheerio.load(body)
+  }
+}
+
+function processarDados(dados){
+  //salva no banco de dados
+  console.log(JSON.stringify(dados))
+}
+
+rp(options)
+.then(($) => {
+  const times = []
+  $('.tabela-body-linha').each((i, item) => {
+
+    const time = {
+      nome: $(item).find('.tabela-times-time-nome').text(),
+      posicao: parseInt($(item).find('.tabela-times-posicao').text())
+    }
+
+    if(time.nome !== "")
+      times.push(time)
+  })
+  processarDados(times)
+})
+.catch((err) => {
+  console.log(err);
+})
